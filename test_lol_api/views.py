a9767a86374ebd4b76fa75f276ee71b1ac456816
daniel <daniel.lim@igaworks.com>
2022-04-27 13:40:55 +0900

    feat: update status html and collect statics

--- test_lol_api/views.py
+++ test_lol_api/views.py
@@ -9,5 +9,23 @@ import requests
 def test_api(request) :
     riot_status_url = "https://kr.api.riotgames.com/lol/status/v4/platform-data"
     response = requests.get(riot_status_url, headers={"X-Riot-Token": key}) 
-    return render(request, 'status.html', {"statusDict" : response.json()})
+    statusDict = response.json()
+    maintenanceList = []
+    for maintenances in statusDict['maintenances'] :
+        maintenanceList.append(maintenances)
+        for updates in maintenanceList[-1]['updates'] :
+            for content in updates['translations']: 
+                if content['locale'] == 'ko_KR' :
+                    updateDict = {
+                        'content' : content['content'],
+                        'locale' : content['locale']
+                    }
+                    updateDict.update(updates)
+                    updateDict.pop('translations')
+                    break
+        maintenanceList[-1]['updates'] = updateDict
+    statusDict['maintenances'] = maintenanceList
+                
+
+    return render(request, 'status.html', {"statusDict" : statusDict})
     # return JsonResponse({"riot_server_status" : response.json()}, json_dumps_params={'ensure_ascii':False})
